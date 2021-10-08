63cc99292244419afc793f3839597979215ad7bb
CharlesHuss <charleshuss360@gmail.com>
2021-10-08 11:56:40 +0200

    new new

--- script/FPS_COUNTER.gd
+++ script/FPS_COUNTER.gd
@@ -3,3 +3,9 @@ extends Spatial
 
 func _process(delta):
 	$FPS_COUNTER.text = str(Engine.get_frames_per_second())
+	
+	if Input.is_action_pressed("escape"):
+		get_tree().paused = true
+		$pause_popup.popup()
+		
+	
