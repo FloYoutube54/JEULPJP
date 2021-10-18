23617eb6b968bdc85273dd50db266a451baedfe3
CharlesHuss <charleshuss360@gmail.com>
2021-10-18 10:34:46 +0200

    J'ai modifié plein de choses
    J'ai modifié plein de trucs et tout...

--- script/pause_popup.gd
+++ script/pause_popup.gd
@@ -4,20 +4,20 @@ extends Popup
 func _ready():
 	pass
 	
-func _process(delta):
-	if Input.is_action_pressed("escape"):
+func _process(_delta):
+	
+	if Input.is_action_just_pressed("escape"):
 		get_tree().paused = !get_tree().paused
 		self.visible = !self.visible
-		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
-
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_of_pause_button_up():
-	get_tree().paused = false
-	$pause_popup.hide()
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_tree().paused = !get_tree().paused
+	self.visible = !self.visible
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_Option_button_up():
-	$pause_popup.hide()
+	self.visible = !self.visible
 	$option_popup.popup()
 
 func _on_leave_button_up():
